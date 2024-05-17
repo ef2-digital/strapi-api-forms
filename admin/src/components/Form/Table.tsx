1babd0d5e80b3ed2f796bc5eb7d53386c3840424
Daan de Gooijer <daan@ef2.nl>
2024-05-17 09:55:38 +0200

    Fixed sorting and active in future

--- admin/src/components/Form/Table.tsx
+++ admin/src/components/Form/Table.tsx
@@ -1,208 +1,186 @@
-import * as React from "react";
-import { useIntl } from "react-intl";
-import pluginId from "../../pluginId";
-import Dialog from "../../components/Dialog";
-import { useHistory } from "react-router-dom";
-import { useState, useContext } from "react";
+import * as React from 'react';
+import { useIntl } from 'react-intl';
+import pluginId from '../../pluginId';
+import Dialog from '../../components/Dialog';
+import { useHistory } from 'react-router-dom';
+import { useState, useContext } from 'react';
 
 import {
-  IconButtonGroup,
-  IconButton,
-  Table as StrapiTable,
-  Thead,
-  Tbody,
-  Tr,
-  Td,
-  Th,
-  Typography,
-  VisuallyHidden,
-} from "@strapi/design-system";
+	IconButtonGroup,
+	IconButton,
+	Icon,
+	Table as StrapiTable,
+	Thead,
+	Tbody,
+	Tr,
+	Td,
+	Th,
+	Typography,
+	VisuallyHidden,
+} from '@strapi/design-system';
 
 //@ts-ignore
-import { Trash, Pencil, Mail, Bell, PaperPlane } from "@strapi/icons";
-import formRequests from "../../api/form";
-import NotificationModal from "../Modal/NotificationModal";
-import { HandlerTypeEnum } from "../../utils/enums";
-import { FormContext } from "../../hooks/useForm";
-import { FormType } from "../../utils/types";
-import { Types } from "../../hooks/formReducer";
-import ExportButton from "../Submission/ExportButton";
+import { Trash, Pencil, Mail, Bell, PaperPlane } from '@strapi/icons';
+import formRequests from '../../api/form';
+import NotificationModal from '../Modal/NotificationModal';
+import { HandlerTypeEnum } from '../../utils/enums';
+import { FormContext } from '../../hooks/useForm';
+import { FormType } from '../../utils/types';
+import { Types } from '../../hooks/formReducer';
+import ExportButton from '../Submission/ExportButton';
+import { CheckCircle } from '@strapi/icons';
+import { CrossCircle } from '@strapi/icons';
 
 const Table = () => {
-  const history = useHistory();
-  const { formatMessage, formatDate } = useIntl();
-  const { dispatch, state } = useContext(FormContext);
+	const history = useHistory();
+	const { formatMessage, formatDate } = useIntl();
+	const { dispatch, state } = useContext(FormContext);
 
-  const [isVisible, setIsVisible] = useState(false);
-  const [isModalVisible, setModalIsVisible] = useState(false);
-  const [notificationType, setSelectedNotificationType] =
-    useState<null | HandlerTypeEnum>(null);
+	const [isVisible, setIsVisible] = useState(false);
+	const [isModalVisible, setModalIsVisible] = useState(false);
+	const [notificationType, setSelectedNotificationType] = useState<null | HandlerTypeEnum>(null);
 
-  const openConfirmModal = (form: FormType) => {
-    dispatch({
-      type: Types.Set_Form,
-      payload: {
-        form: form,
-      },
-    });
+	const openConfirmModal = (form: FormType) => {
+		dispatch({
+			type: Types.Set_Form,
+			payload: {
+				form: form,
+			},
+		});
 
-    setIsVisible(true);
-  };
+		setIsVisible(true);
+	};
 
-  const openNotificationModal = (form: FormType, type: HandlerTypeEnum) => {
-    dispatch({
-      type: Types.Set_Form,
-      payload: {
-        form: form,
-      },
-    });
+	const openNotificationModal = (form: FormType, type: HandlerTypeEnum) => {
+		dispatch({
+			type: Types.Set_Form,
+			payload: {
+				form: form,
+			},
+		});
 
-    setModalIsVisible(true);
-    setSelectedNotificationType(type);
-  };
+		setModalIsVisible(true);
+		setSelectedNotificationType(type);
+	};
 
-  const deleteForm = async () => {
-    formRequests.deleteForm(state.form.id!).then(() => {
-      setIsVisible(false);
-    });
-  };
+	const deleteForm = async () => {
+		formRequests.deleteForm(state.form.id!).then(() => {
+			setIsVisible(false);
+		});
+	};
 
-  return (
-    <>
-      <StrapiTable>
-        <Thead>
-          <Tr>
-            {[
-              "#",
-              formatMessage({
-                id: `${pluginId}.list.name`,
-              }),
-              formatMessage({
-                id: `${pluginId}.list.creation_date`,
-              }),
-              formatMessage({
-                id: `${pluginId}.list.submissions`,
-              }),
-              formatMessage({
-                id: `${pluginId}.list.handlers`,
-              }),
+	return (
+		<>
+			<StrapiTable>
+				<Thead>
+					<Tr>
+						{[
+							'#',
+							formatMessage({
+								id: `${pluginId}.list.name`,
+							}),
 
-              <VisuallyHidden>Actions</VisuallyHidden>,
-            ].map((heading, index) => (
-              <Th key={`heading-${index}`}>
-                <Typography variant="sigma">{heading}</Typography>
-              </Th>
-            ))}
-          </Tr>
-        </Thead>
-        <Tbody>
-          {Boolean(state.forms.length) &&
-            state.forms.map((form: FormType) => (
-              <Tr key={`${form.id}-form`}>
-                <Td>{form.id}</Td>
-                <Td>
-                  <Typography textColor="neutral800">
-                    {form.attributes.title}
-                  </Typography>
-                </Td>
+							formatMessage({
+								id: `${pluginId}.list.creation_date`,
+							}),
+							formatMessage({
+								id: `${pluginId}.list.submissions`,
+							}),
+							formatMessage({
+								id: `${pluginId}.list.handlers`,
+							}),
 
-                <Td>
-                  <Typography textColor="neutral800">
-                    {formatDate(form.attributes.createdAt, {
-                      dateStyle: "full",
-                      timeStyle: "short",
-                    })}
-                  </Typography>
-                </Td>
-                <Td>
-                  <IconButtonGroup>
-                    {Boolean(form.attributes?.submissions!.length) && (
-                      <ExportButton formId={form.id!} />
-                    )}
-                    <IconButton
-                      onClick={() =>
-                        history.push(
-                          `/plugins/${pluginId}/submission/list/${form.id}`
-                        )
-                      }
-                      label={formatMessage({
-                        id: `${pluginId}.forms.fields.actions.submissions`,
-                      })}
-                      icon={<PaperPlane />}
-                    />
-                  </IconButtonGroup>
-                </Td>
-                <Td>
-                  <IconButtonGroup justifyContent="start">
-                    {Boolean(form.attributes?.notifications!.length) &&
-                      form.attributes?.notifications!.map((notification) => (
-                        <IconButton
-                          onClick={() =>
-                            openNotificationModal(
-                              form,
-                              notification.identifier as HandlerTypeEnum
-                            )
-                          }
-                          label={formatMessage({
-                            id: `${pluginId}.forms.fields.actions.${notification.identifier}`,
-                          })}
-                          icon={
-                            notification.identifier ===
-                            HandlerTypeEnum.Notification ? (
-                              <Bell />
-                            ) : (
-                              <Mail />
-                            )
-                          }
-                        />
-                      ))}
-                  </IconButtonGroup>
-                </Td>
-                <Td>
-                  <IconButtonGroup justify="end">
-                    <IconButton
-                      onClick={() =>
-                        history.push(
-                          `/plugins/${pluginId}/form/edit/${form.id}`
-                        )
-                      }
-                      label={formatMessage({
-                        id: `${pluginId}.forms.fields.actions.edit`,
-                      })}
-                      icon={<Pencil />}
-                    />
-                    <IconButton
-                      onClick={() => openConfirmModal(form)}
-                      label={formatMessage({
-                        id: `${pluginId}.forms.fields.actions.remove`,
-                      })}
-                      icon={<Trash />}
-                    />
-                  </IconButtonGroup>
-                </Td>
-              </Tr>
-            ))}
-        </Tbody>
-      </StrapiTable>
+							<VisuallyHidden>Actions</VisuallyHidden>,
+							<VisuallyHidden>Active</VisuallyHidden>,
+						].map((heading, index) => (
+							<Th key={`heading-${index}`}>
+								<Typography variant="sigma">{heading}</Typography>
+							</Th>
+						))}
+					</Tr>
+				</Thead>
+				<Tbody>
+					{Boolean(state.forms.length) &&
+						state.forms.map((form: FormType) => (
+							<Tr key={`${form.id}-form`}>
+								<Td>{form.id}</Td>
+								<Td>
+									<Typography textColor="neutral800">{form.attributes.title}</Typography>
+								</Td>
+								<Td>
+									<Typography textColor="neutral800">
+										{formatDate(form.attributes.createdAt, {
+											dateStyle: 'full',
+											timeStyle: 'short',
+										})}
+									</Typography>
+								</Td>
+								<Td>
+									<IconButtonGroup>
+										{Boolean(form.attributes?.submissions!.length) && <ExportButton formId={form.id!} />}
+										<IconButton
+											onClick={() => history.push(`/plugins/${pluginId}/submission/list/${form.id}`)}
+											label={formatMessage({
+												id: `${pluginId}.forms.fields.actions.submissions`,
+											})}
+											icon={<PaperPlane />}
+										/>
+									</IconButtonGroup>
+								</Td>
+								<Td>
+									<IconButtonGroup justifyContent="start">
+										{Boolean(form.attributes?.notifications!.length) &&
+											form.attributes?.notifications!.map((notification) => (
+												<IconButton
+													onClick={() => openNotificationModal(form, notification.identifier as HandlerTypeEnum)}
+													label={formatMessage({
+														id: `${pluginId}.forms.fields.actions.${notification.identifier}`,
+													})}
+													icon={notification.identifier === HandlerTypeEnum.Notification ? <Bell /> : <Mail />}
+												/>
+											))}
+									</IconButtonGroup>
+								</Td>
+								<Td>
+									<IconButtonGroup justify="end">
+										<IconButton
+											onClick={() => history.push(`/plugins/${pluginId}/form/edit/${form.id}`)}
+											label={formatMessage({
+												id: `${pluginId}.forms.fields.actions.edit`,
+											})}
+											icon={<Pencil />}
+										/>
+										<IconButton
+											onClick={() => openConfirmModal(form)}
+											label={formatMessage({
+												id: `${pluginId}.forms.fields.actions.remove`,
+											})}
+											icon={<Trash />}
+										/>
+									</IconButtonGroup>
+								</Td>
+								<Td>
+									{form.attributes.active ? <Icon as={CheckCircle} color="success500" /> : <Icon as={CrossCircle} color="danger500" />}
+								</Td>
+							</Tr>
+						))}
+				</Tbody>
+			</StrapiTable>
 
-      {isModalVisible && (
-        <NotificationModal
-          type={notificationType!}
-          isModalVisible={isModalVisible}
-          setModalIsVisible={setModalIsVisible}
-        />
-      )}
-      <Dialog
-        setIsVisible={setIsVisible}
-        confirmAction={deleteForm}
-        isVisible={isVisible}
-        description={formatMessage({
-          id: `${pluginId}.dialog.delete.description`,
-        })}
-        title={formatMessage({ id: `${pluginId}.dialog.delete.text` })}
-      />
-    </>
-  );
+			{isModalVisible && (
+				<NotificationModal type={notificationType!} isModalVisible={isModalVisible} setModalIsVisible={setModalIsVisible} />
+			)}
+			<Dialog
+				setIsVisible={setIsVisible}
+				confirmAction={deleteForm}
+				isVisible={isVisible}
+				description={formatMessage({
+					id: `${pluginId}.dialog.delete.description`,
+				})}
+				title={formatMessage({ id: `${pluginId}.dialog.delete.text` })}
+			/>
+		</>
+	);
 };
 
 export default Table;
--- admin/src/pages/FormList/index.tsx
+++ admin/src/pages/FormList/index.tsx
@@ -1,72 +1,67 @@
-import * as React from "react";
-import Header from "../../components/Form/Header";
-import { useIntl } from "react-intl";
-import pluginId from "../../pluginId";
-import { useEffect, useContext } from "react";
-import formRequests from "../../api/form";
-import { FormResponse } from "../../utils/types";
-import { default as FormTable } from "../../components/Form/Table";
-import { Box, Grid, GridItem, Flex, LinkButton } from "@strapi/design-system";
-import { Plus } from "@strapi/icons";
-import { NavLink } from "react-router-dom";
-import { FormContext } from "../../hooks/useForm";
-import { Types } from "../../hooks/formReducer";
-const qs = require("qs");
+import * as React from 'react';
+import Header from '../../components/Form/Header';
+import { useIntl } from 'react-intl';
+import pluginId from '../../pluginId';
+import { useEffect, useContext } from 'react';
+import formRequests from '../../api/form';
+import { FormResponse } from '../../utils/types';
+import { default as FormTable } from '../../components/Form/Table';
+import { Box, Grid, GridItem, Flex, LinkButton } from '@strapi/design-system';
+import { Plus } from '@strapi/icons';
+import { NavLink } from 'react-router-dom';
+import { FormContext } from '../../hooks/useForm';
+import { Types } from '../../hooks/formReducer';
+const qs = require('qs');
 
 const FormList = () => {
-  const { formatMessage } = useIntl();
-  const { dispatch, state } = useContext(FormContext);
+	const { formatMessage } = useIntl();
+	const { dispatch, state } = useContext(FormContext);
 
-  useEffect(() => {
-    formRequests
-      .getForms(
-        qs.stringify(
-          { "pagination[page]": 1, "pagination[pageSize]": 9999 },
-          {
-            encodeValuesOnly: true,
-          }
-        )
-      )
-      .then((response: FormResponse) => {
-        dispatch({
-          type: Types.Set_Forms,
-          payload: {
-            forms: response.data,
-          },
-        });
-      });
-  }, []);
+	useEffect(() => {
+		formRequests
+			.getForms(
+				qs.stringify(
+					{ 'pagination[page]': 1, 'pagination[pageSize]': 9999, sort: 'id:desc' },
+					{
+						encodeValuesOnly: true,
+					}
+				)
+			)
+			.then((response: FormResponse) => {
+				dispatch({
+					type: Types.Set_Forms,
+					payload: {
+						forms: response.data,
+					},
+				});
+			});
+	}, []);
 
-  return (
-    <>
-      <Header
-        title={formatMessage({ id: `${pluginId}.forms.label` })}
-        subtitle=""
-        backUrl={`/plugins/${pluginId}`}
-        backTitle={formatMessage({ id: `${pluginId}.back_to_dashboard` })}
-      />
-      <Box paddingLeft={10} paddingRight={10} paddingBottom={5}>
-        <Flex justifyContent="space-between">
-          <LinkButton
-            style={{ textDecoration: "none" }}
-            startIcon={<Plus />}
-            as={NavLink}
-            to={`/plugins/${pluginId}/form/add`}
-          >
-            {formatMessage({ id: `${pluginId}.heading.add` })}
-          </LinkButton>
-        </Flex>
-      </Box>
+	return (
+		<>
+			<Header
+				title={formatMessage({ id: `${pluginId}.forms.label` })}
+				subtitle=""
+				backUrl={`/plugins/${pluginId}`}
+				backTitle={formatMessage({ id: `${pluginId}.back_to_dashboard` })}
+			/>
+			<Box paddingLeft={10} paddingRight={10} paddingBottom={5}>
+				<Flex justifyContent="space-between">
+					<LinkButton style={{ textDecoration: 'none' }} startIcon={<Plus />} as={NavLink} to={`/plugins/${pluginId}/form/add`}>
+						{formatMessage({ id: `${pluginId}.heading.add` })}
+					</LinkButton>
+				</Flex>
+			</Box>
 
-      <Grid>
-        <GridItem col={10} s={12}>
-          <Box paddingLeft={10} paddingRight={10}>
-            {state.forms.length > 0 ? <FormTable /> : <></>}
-          </Box>
-        </GridItem>
-      </Grid>
-    </>
-  );
+			<Grid>
+				<GridItem col={10} s={12}>
+					<Box paddingLeft={10} paddingRight={10}>
+						{state.forms.length > 0 ? <FormTable /> : <></>}
+					</Box>
+				</GridItem>
+			</Grid>
+		</>
+	);
 };
 
 export default FormList;
